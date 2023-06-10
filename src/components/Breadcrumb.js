a233a81274d81cb739701794911344b69d1e7d69
sajankota <sajankota001@gmail.com>
2023-06-10 09:43:54 +0530

    first commit

---
+++ src/components/Breadcrumb.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Breadcrumb = ({ title }) => {
+  return (
+    <>
+      {/*  ================ Breadcrumb Start ================  */}
+      <div
+        className="breadcrumb-area bg-cover"
+        style={{ backgroundImage: 'url("./assets/img/bg/7.png")' }}
+      >
+        <div className="container">
+          <div className="breadcrumb-inner">
+            <div className="row justify-content-center">
+              <div className="col-lg-6">
+                <h2 className="page-title">{title}</h2>
+              </div>
+              <div className="col-lg-6 text-lg-end">
+                <ul className="page-list">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>{title}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*  ================ Breadcrumb End ================  */}
+    </>
+  );
+};
+
+Breadcrumb.propTypes = {
+  title: String.isRequired
+};
+export default Breadcrumb;
